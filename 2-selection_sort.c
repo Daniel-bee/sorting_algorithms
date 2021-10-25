84091362de506ed04c99d2ad0457d85eb3e73bcf
Daniel-bee <daniel123_86@live.com>
2021-10-25 19:13:24 +0300

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,7 +10,7 @@ void selection_sort(int *array, size_t size)
 {
 	unsigned int i, j, min;
 
-	if (array == NULL  || size < 2)
+	if (array == NULL  || size == 0)
 		return;
 
 	for (i = 0; i < size - 1; i++)
