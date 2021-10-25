cc27a8e2ad21764118469b61480a574d81f6ba61
Daniel-bee <daniel123_86@live.com>
2021-10-25 12:06:52 +0300

    Inserion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,6 @@
 #include "sort.h"
-#include "exchange.c"
+void display(listint_t *);
+void exchangeData(listint_t *, listint_t *);
 /**
  * insertion_sort_list - sorts a doubly linked list of integers in
  * ascending order using the Insertion sort algorithm
@@ -23,4 +24,64 @@ void insertion_sort_list(listint_t **list)
 	}
 	while ((*list)->prev != NULL)
 		*list = (*list)->prev;
+}
+/**
+ * display - display ddl list
+ * @head: list
+ */
+void display(listint_t *head)
+{
+	listint_t *trav;
+
+	trav = head;
+	while (trav->prev != NULL)
+		trav = trav->prev;
+	print_list(trav);
+
+}
+/**
+ * exchangeData - Exchange two nodes tail and head position
+ * @list: list
+ * @temp: temp list
+ **/
+void exchangeData(listint_t *list, listint_t *temp)
+{
+	temp = list->prev;
+	if (temp != NULL)
+	{
+		temp->next = list->next;
+		list->next->prev = temp;
+	}
+	else
+		list->next->prev = NULL;
+	list->prev = list->next;
+	if (list->next->next != NULL)
+	{
+		list->next = list->next->next;
+		list->prev->next = list;
+		list->next->prev = list;
+	}
+	else
+	{
+		list->next->next = list;
+		list->next = NULL;
+	}
+	display(list);
+	while (list->prev != NULL)
+	{
+		if (list->n < list->prev->n)
+		{
+			temp = list->prev->prev;
+			list->prev->next = list->next;
+			list->next = list->prev;
+			list->prev->prev = list;
+			list->prev = temp;
+			list->next->next->prev = list->next;
+			if (temp != NULL)
+				temp->next = list;
+			display(list);
+		}
+		else
+			list = list->prev;
+	}
 }
