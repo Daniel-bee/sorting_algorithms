3de93603d3a20d9bff3e8a872988f48c5e132acd
Daniel-bee <84592600+Daniel-bee@users.noreply.github.com>
2021-10-25 11:46:49 +0300

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,21 +9,24 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j, l, flag = 0;
 
-	for (i = 0; i < size - 1; i++)
+	if (!array || !size)
 	{
-		for (j = 0; j < size - 1;  j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < size - 1;  j++)
 			{
-				exchange(array, j, j + 1);
-				flag = 1;
-				for (l = 0; l < size - 1; l++)
+				if (array[j] > array[j + 1])
+				{
+					exchange(array, j, j + 1);
+					flag = 1;
+					for (l = 0; l < size - 1; l++)
 					printf("%d, ", array[l]);
-				printf("%d\n", array[size - 1]);
+					printf("%d\n", array[size - 1]);
+				}
 			}
+			if (flag == 0)
+				break;
 		}
-		if (flag == 0)
-			break;
 	}
 }
 /**
