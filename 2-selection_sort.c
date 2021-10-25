cabe4115d65ca626ae91daa5042921687d3f649c
Daniel-bee <daniel123_86@live.com>
2021-10-25 16:06:47 +0300

    Selection soorting

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,30 +12,22 @@ void selection_sort(int *array, size_t size)
 
 	if (array == NULL  || size == 0)
 		return;
-	while (i < size - 1)
+	while (i < size -1)
 	{
 		min = i;
 		j = i + 1;
 		while (j < size)
 		{
 			if (array[j] < array[min])
-			{
 				min = j;
-			}
-			j++;
-		}
-		if (min != i)
-		{
-			switch_(array, min, i);
-		}
-		j = 0;
-		while (j < size - 1)
-		{
-			printf("%d, ", array[j]);
-			j++;
-		}
-		printf("%d\n", array[size - 1]);
 
+			if (min != i)
+			{
+				switch_(array, min, i);
+				print_array(array, size);
+			}
+		j++;
+		}
 		i++;
 	}
 }
